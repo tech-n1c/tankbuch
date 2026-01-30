19a69ffee129abb870c153df268ebd912c2a1f6e
tech-n1c <nicolas.maiwald@outlook.de>
2026-01-30 23:17:58 +0100

    init

---
+++ tankbuch-backend/src/main/java/cc/maiwald/tankbuchbackend/TankbuchBackendApplication.java
@@ -0,0 +1,13 @@
+package cc.maiwald.tankbuchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankbuchBackendApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TankbuchBackendApplication.class, args);
+    }
+
+}
---
+++ tankbuch-backend/src/test/java/cc/maiwald/tankbuchbackend/TankbuchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.maiwald.tankbuchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankbuchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
