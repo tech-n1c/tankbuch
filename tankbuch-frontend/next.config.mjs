78df8659455b2e60433b65dec1b9ce86ee8f570a
tech-n1c <nicolas.maiwald@outlook.de>
2026-01-31 13:54:23 +0100

    refactor(next): remove experimental runtime configuration

--- tankbuch-frontend/next.config.mjs
+++ tankbuch-frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    runtime: 'nodejs'
-  }
+  output: 'standalone'
 }
 
 export default nextConfig
